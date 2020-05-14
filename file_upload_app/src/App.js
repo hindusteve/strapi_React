e57f20e86b68764c4e076483cb6370e75ba29d0d
599746 <abhijitkhanna@C02WW36LHTDD.fios-router.home>
2020-05-14 19:46:08 -0400

    added FileUpload component to App.js

--- file_upload_app/src/App.js
+++ file_upload_app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FileUpload from './components/FileUpload'
+
 
 function App() {
   return (
     <div className="App">
-
+      <FileUpload/>
     </div>
   );
 }
